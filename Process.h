011e86a32cfd9575135f229c68f5a80f457befbe
jizhouh <48373149+jizhouh@users.noreply.github.com>
2019-03-22 14:51:00 -0400

    SRT TEMP

--- Process.h
+++ Process.h
@@ -3,53 +3,23 @@
 #include<vector>
 using namespace std;
 
-/*I did some adjustment, please see submitty's project assignment page 5*/
-/*For running the process, you can still do as our discussed but need call IsBurstFinished() at every run end so new cpu burst can be update*/
-/*Please tracking the state to make sure assign the porcess correctly*/
-class Process{
+class Process {
 
-	public:
-		char name;
-		int state;      //1:read; 2:running: 3:block
-		int number_of_burst;   //range from [1,100]
-		int tau;
-		vector<int> io_time;   //number of io time is one less than cpu as process always end with cpu burst
-		vector<int> cpu_burst; //number of cpu burst time arrange from [1,100], order from first arrive to the last arrive
-		vector<int> interarrival; //the arrival time of each burst, some order as the cpu burst
+public:
 
-		
-		int arrive_time; //process arrive time
-		int wait_time;
-                int turnaround_time;
-                int preemptive;
+	int arrive_time; //process arrive time
+	int wait_time;
+	int turnaround_time;
+	char name;
+	int cpu_remain;
+	int io_remain;
 
-		int cpu_remain;
-		int io_remain;
-		
-		int cpu_ptr; //indicate which cpu burst the process currently in 
-		int io_ptr; //indicate which io burst the process currently in
-
-	        bool finished;	
-
-                /*Constructor*/
-		Process(); //default constructor for map function 
-		Process(char Name,float lamda,float limit); //seed for generating all these time and place
-		Process(const Process &p); //copy
-
-		/*Mutator*/
-		int update(int state); //update the remain time of burst, 1 or 2 will update cpu_remain and set state to 3
-		                       //3 will update io_remain and set state 1
-				       //return -1 if fail, else return the new state of process
-
-		/*Observer*/
-		bool IsProcessFinished() const; //true if processfinished, false else
-		bool IsCPUFinished() const;//true if state is 1 or 2 and cpu remain == 0, false if the state is 3 or cpu_remain != 0
-		bool IsIOFinished() const;// true if state is 3 and io_remain == 0; false else 
-		bool IsArrive(int time) const;//true if a process/burst arrive, false else
-		void output(); //print all results for debugging
-
-	private:
-		void time_generation(float limit, float lamda);
-
+	Process(int arr, int wait, int turn, int cpu, int io, char n) {
+		arrive_time = arr;
+		wait_time = wait;
+		turnaround_time = turn;
+		name = n;
+		cpu_remain = cpu;
+		io_remain = io;
+	}
 };
-
---
+++ method.cpp
@@ -0,0 +1,186 @@
+#include <iostream>
+#include <string>
+#include <vector>
+#include <queue>
+#include "Process.h"
+
+using namespace std;
+void srt(int &, int, vector<Process> &, vector<char> &, int &, int &, int &, char &);
+
+
+int main() {
+	vector<Process> p;
+	for (int i = 0; i < 10; i++) {
+		Process temp = Process((i+1) * 10, 0, 0, (i + 1) * 5, (i + 1) * 4, 'A');
+		p.push_back(temp);
+		cout << "arrive: " << p[i].arrive_time << " io: " << p[i].io_remain << " cpu: " << p[i].cpu_remain << " name: " << p[i].name << endl;
+	}
+	for (int i = 0; i < 10; i++) {
+		Process temp = Process((i + 1) * 10+5, 0, 0, (i + 1) * 10+5, (i + 1) * 4+5, 'B');
+		p.push_back(temp);
+		cout << "arrive: " << p[i+10].arrive_time << " io: " << p[i+10].io_remain << " cpu: " << p[i+10].cpu_remain << " name: " << p[i + 10].name << endl;
+	}
+	vector<char> q;
+	int t = 0, pre = 0, tau = 0, io = -1;
+	char in = ' ';
+	while (t < 1200) {
+		srt(t, 0, p, q, pre, tau, io, in);
+		t++;
+	}
+	return 0;
+}
+
+void srt(int &Time, int tcs, vector<Process> &total, vector<char> &ready_queue, int &pre_index, int &tau, int &io_finish, char &in_io) {
+	if (Time == io_finish) {
+		ready_queue.push_back(in_io);
+		in_io = ' ';
+	}
+	/*Add process to queue*/
+	for (int i = 0; i < total.size(); i++) {
+		if (total[i].arrive_time == Time) {
+			int check_multiple = 0;
+			for (int k = 0; k < ready_queue.size(); k++) {
+				if (total[i].name == ready_queue[k]) {
+					check_multiple = 1;
+				}
+			}
+			if (check_multiple == 0) {
+				ready_queue.push_back(total[i].name);
+			}
+			cout << "time " << Time << "ms: Process " << total[i].name;
+			cout << " (tau " << tau << "ms) arrived: added to ready queue [Q";
+			for (int j = 0; j < ready_queue.size(); j++) {
+				char temp = ready_queue[j];
+				cout << " " << temp;
+			}
+			cout << "]" << endl;
+			if (ready_queue.size() == 0) {
+				cout << "<empty>";
+			}
+		}
+	}
+
+	/*Find the shortest remain time of arrived process and record its index*/
+	int index = 0;
+	//Process smallest_remain = null;
+	for (int i = 0; i < total.size(); i++) {
+		if (total[i].arrive_time <= Time && total[i].cpu_remain > 0 && total[i].name != in_io) {
+			//Process smallest_remain = total[i];
+			index = i;
+			for (int j = i + 1; j < total.size(); j++) {
+				if (total[j].arrive_time <= Time && total[j].cpu_remain > 0) {
+					if (total[j].cpu_remain < total[i].cpu_remain && total[i].name != in_io) {
+						//smallest_remain = total[j];
+						index = j;
+					}
+				}
+			}
+		}
+	}
+
+
+	/*pop_back out the shortest remain time process*/
+	int check_queue = 0;
+	for (int j = 0; j < ready_queue.size(); j++) {
+		if (ready_queue[j] == total[index].name) {
+			ready_queue.erase(ready_queue.begin()+j);
+		}
+	}
+
+	bool context_switched = false;
+	/*Check context-switch*/
+	if (index != pre_index || Time == total[index].arrive_time) {
+		context_switched = true;
+	}
+	else {
+		context_switched = false;
+	}
+
+	/*Check preemption*/
+	bool preemption = false;
+	if (total[pre_index].name != total[index].name) {
+		preemption = true;
+	}
+
+	/*Start Processing...*/
+	if (context_switched == false && total[index].arrive_time <= Time) {
+		total[index].cpu_remain--;
+		//total[index].io_remain--;
+		total[index].turnaround_time = Time - total[index].arrive_time;
+	}
+	else if (context_switched == true && preemption == false) {
+		Time += tcs / 2;
+		cout << "time " << Time << "ms: Process " << total[index].name << " started using the CPU for ";
+		cout << total[index].cpu_remain << "ms burst [Q ";
+		for (int j = 0; j < ready_queue.size(); j++) {
+			char temp = ready_queue[j];
+			cout << " " << temp;
+		}
+		if (ready_queue.size() == 0) {
+			cout << "<empty>";
+		}
+		cout << "]" << endl;
+	}
+
+	/*When CPU burst is finished...*/
+	if (total[index].cpu_remain <= 0) {
+		
+		/*Start to print complete message*/
+		cout << "time " << Time << "ms: Process " << total[index].name << " completed a CPU burst; ";
+		cout << 15 << " bursts to go [Q";
+		for (int j = 0; j < ready_queue.size(); j++) {
+			char temp = ready_queue[j];
+			cout << " " << temp;
+		}
+		if (ready_queue.size() == 0) {
+			cout << "<empty>";
+		}
+		cout << "]" << endl;
+		/*Recalculate tau*/
+		tau = 100; //To be changed
+		cout << "time " << Time << "ms: Recalculated tau = " << tau << "ms for process " << total[index].name << " [Q";
+		for (int j = 0; j < ready_queue.size(); j++) {
+			char temp = ready_queue[j];
+			cout << " " << temp;
+		}
+		if (ready_queue.size() == 0) {
+			cout << "<empty>";
+		}
+		cout << "]" << endl;
+
+		/*Start to print i/o message*/
+		cout << "time " << Time << "ms: Process " << total[index].name << " switching out of CPU; will block on I/O until time ";
+		cout << Time + total[index].io_remain << "ms [Q";
+		io_finish = Time + total[index].io_remain;
+		in_io = total[index].name;
+		for (int j = 0; j < ready_queue.size(); j++) {
+			char temp = ready_queue[j];
+			cout << " " << temp;
+		}
+		if (ready_queue.size() == 0) {
+			cout << "<empty>";
+		}
+		cout << "]" << endl;
+
+		/*Exit time*/
+		Time += tcs / 2;
+	}
+	/*When preemption is happened...*/
+	if (preemption == true) {
+		cout << "time " << Time << "ms: Process " << total[pre_index].name << " (tau  " << tau << "ms) completed I/O and will preempt ";
+		cout << total[index].name << " [Q ";
+		for (int j = 0; j < ready_queue.size(); j++) {
+			char temp = ready_queue[j];
+			cout << " " << temp;
+		}
+		if (ready_queue.size() == 0) {
+			cout << "<empty>";
+		}
+		cout << "]" << endl;
+		Time += tcs;
+	}
+
+	/*Change previous index to current index for the next iteration*/
+	pre_index = index;
+
+}
